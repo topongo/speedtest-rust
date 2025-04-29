a21b7e9535aebcf5887d237fd5bf3f7926ec166b
Lorenzo Bodini <lorenzo.bodini@prabo.org>
2025-04-29 16:29:58 +0200

    customization

--- assets/servers_list.js
+++ assets/servers_list.js
@@ -2,7 +2,7 @@ function get_servers() {
     // your server list here
     return [
         {
-            name : "Your server name here",
+            name : "prabo.org",
             server : window.location.origin,
             dlURL: "backend/garbage", // edit this when you changed base_url in configs file
             ulURL: "backend/empty", // edit this when you changed base_url in configs file
